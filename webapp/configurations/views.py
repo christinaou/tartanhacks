f688b2737fd94450fe2e40754a0447269e13e5fe
Kristin Yin <kristinyin@hotmail.com>
2018-02-10 00:18:05 -0500

    added triggers

--- webapp/configurations/views.py
+++ webapp/configurations/views.py
@@ -3,12 +3,32 @@ from django.http import HttpResponseRedirect
 from django.template import loader
 from django.http import HttpResponse
 from django.views.decorators.csrf import csrf_exempt
-
+triggers = [
+    {
+        'number': 9492059539,
+        'word': "apple",
+        'type': "call",
+        'id':0
+    }
+]
 
 @csrf_exempt
 def index(request):
     if request.method=="POST":
-        data = request.POST["name_field"]
-        print(data)
+        # Delete trigger lol
+        if "trigger_id" in request.POST:
+            trigger_id = request.POST["trigger_id"]
+            triggers.pop(int(trigger_id))
+        # Add trigger
+        else:
+            number = request.POST["trigger_number"]
+            word = request.POST['trigger_word']
+            trigger_type = request.POST['trigger_type']
+            triggers.append({
+                'number':number,
+                'word': word,
+                'type': trigger_type,
+                'id': len(triggers)
+            })
     template = loader.get_template("configurations/index.html")
-    return HttpResponse(template.render())
+    return HttpResponse(template.render({'triggers':triggers}))
