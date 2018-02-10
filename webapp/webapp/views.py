727554f1c30c22d3975dff4da3ebabfe6a75caec
Kristin Yin <kristinyin@hotmail.com>
2018-02-10 15:24:07 -0500

    do onboard page

--- webapp/webapp/views.py
+++ webapp/webapp/views.py
@@ -14,13 +14,31 @@ with open(dirpath + '/../database.json') as src:
 @csrf_exempt
 def index(request):
     if request.method=="POST":
-        myName = request.POST['user_name']
-        db['myName'] = myName
+        if "user_name" in request.POST:
+            myName = request.POST['user_name']
+            db['myName'] = myName
+            with open(dirpath + '/../database.json', 'w+') as outfile:
+                json.dump(db, outfile)
+            print(myName)
+            template = loader.get_template("onboard.html")
+        elif "ethnicity" in request.POST:
+            ethnicity = request.POST['ethnicity']
+            gender = request.POST['gender']
+            height_feet = request.POST['height_feet']
+            height_inches = request.POST['height_inches']
+            hair_color = request.POST['hair_color']
+
+            db['ethnicity'] = ethnicity
+            db['gender'] = gender
+            db['height_feet'] = height_feet
+            db['height_inches'] = height_inches
+            db['hair_color'] = hair_color
+            template = loader.get_template("onboard2.html")
+
         with open(dirpath + '/../database.json', 'w+') as outfile:
             json.dump(db, outfile)
-        print(myName)
-        template = loader.get_template("onboard.html")
         return HttpResponse(template.render())
+
 
     template = loader.get_template("index.html")
     return HttpResponse(template.render())
