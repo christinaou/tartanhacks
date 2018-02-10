c2e171d6e77f081c9bb0e6cbb3a97dc33edd5e00
Kristin Yin <kristinyin@hotmail.com>
2018-02-10 00:38:13 -0500

    fix merge conflicts

--- webapp/configurations/views.py
+++ webapp/configurations/views.py
@@ -2,16 +2,11 @@ from django.shortcuts import render
 from django.http import HttpResponseRedirect
 from django.template import loader
 from django.http import HttpResponse
-<<<<<<< HEAD
 from . import call
 from . import stt
 import os
-=======
 from django.views.decorators.csrf import csrf_exempt
-<<<<<<< HEAD
 
->>>>>>> 53645ca9a628785fc833130bd8b2a939d4b6bd25
-=======
 triggers = [
     {
         'number': 9492059539,
@@ -20,17 +15,13 @@ triggers = [
         'id':0
     }
 ]
->>>>>>> triggers
 
 @csrf_exempt
 def index(request):
-<<<<<<< HEAD
     # number = "15103042628"
     # call.main(number)
 
     # stt.main("../test/Sample.wav")
-    return HttpResponse("Hello, world. You're at the configurations index.")
-=======
     if request.method=="POST":
         # Delete trigger lol
         if "trigger_id" in request.POST:
@@ -48,9 +39,4 @@ def index(request):
                 'id': len(triggers)
             })
     template = loader.get_template("configurations/index.html")
-<<<<<<< HEAD
-    return HttpResponse(template.render())
->>>>>>> 53645ca9a628785fc833130bd8b2a939d4b6bd25
-=======
     return HttpResponse(template.render({'triggers':triggers}))
->>>>>>> triggers
