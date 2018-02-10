8dbe0cf4a20894ca316d3ae46ebaad1a140974ab
Kristin Yin <kristinyin@hotmail.com>
2018-02-10 14:30:35 -0500

    make real db

--- webapp/webapp/views.py
+++ webapp/webapp/views.py
@@ -1,11 +1,25 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.views.decorators.csrf import csrf_exempt
 from django.shortcuts import render
 from django.template import loader
+import os
+import json
 
 
+dirpath = os.path.dirname(os.path.realpath(__file__))
+with open(dirpath + '/../database.json') as src:
+    db = json.load(src)
 
+@csrf_exempt
 def index(request):
+    if request.method=="POST":
+        myName = request.POST['user_name']
+        db['myName'] = myName
+        with open(dirpath + '/../database.json', 'w+') as outfile:
+            json.dump(db, outfile)
+        print(myName)
+
     template = loader.get_template("index.html")
     return HttpResponse(template.render())
 def onboard(request):
